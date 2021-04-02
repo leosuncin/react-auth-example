aad28028da8f86be3dcb6063f6f5757dc26c32de
Jaime Leonardo Suncin Cruz <leosuncin@gmail.com>
2021-04-01 22:01:01 -0600

    test: register form component
    Add validation rule for email's pattern.
    Fix adding id to inputs.
    Amend submit handler callback.

--- src/testUtils.tsx
+++ src/testUtils.tsx
@@ -1,10 +1,11 @@
 import { ChakraProvider, theme } from '@chakra-ui/react';
-import { build, fake } from '@jackfranklin/test-data-bot';
+import { build, fake, perBuild } from '@jackfranklin/test-data-bot';
 import { render, screen, waitFor } from '@testing-library/react';
 
 import { AuthProvider } from './hooks/useAuth';
 import type { AuthContext } from './types/AuthContext';
 import type { Login } from './types/Login';
+import type { Register } from './types/Register';
 
 function wrapWithProviders(context: AuthContext = {} as AuthContext) {
   return ({ children }: React.PropsWithChildren<{}>) => (
@@ -33,4 +34,21 @@ const loginBuild = build<Login>({
   },
 });
 
-export { customRender as render, screen, waitFor, loginBuild };
+const registerBuild = build<Register>({
+  fields: {
+    name: fake((f) => f.name.findName()),
+    email: fake((f) => f.internet.email()),
+    password: fake((f) => f.internet.password(12)),
+  },
+  traits: {
+    invalid: {
+      overrides: {
+        name: perBuild(() => Math.random().toString(36).substring(2, 3)),
+        email: fake((f) => f.internet.userName()),
+        password: fake((f) => f.internet.password(7)),
+      },
+    },
+  },
+});
+
+export { customRender as render, screen, waitFor, loginBuild, registerBuild };

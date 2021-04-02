e16ef48cf5d2d0334e7bb4613f1059a0396e6f52
Jaime Leonardo Suncin Cruz <leosuncin@gmail.com>
2021-04-01 20:36:02 -0600

    test: login form component
    Install @jackfranklin/test-data-bot to generate test data.
    Add validation rule for email's pattern.
    Fix adding id to inputs.

---
+++ src/components/LoginForm.test.tsx
@@ -0,0 +1,61 @@
+import userEvent from '@testing-library/user-event';
+
+import { loginBuild, render, screen, waitFor } from '../testUtils';
+import LoginForm, { validations } from './LoginForm';
+
+test('submit login form', async () => {
+  const handleSubmitSpy = jest
+    .fn()
+    .mockImplementation(
+      () => new Promise((resolve) => setTimeout(resolve, 750)),
+    );
+  const data = loginBuild();
+
+  render(<LoginForm onSubmit={handleSubmitSpy} />);
+
+  userEvent.type(screen.getByLabelText(/Email/i), data.email);
+  userEvent.type(screen.getByLabelText(/Password/i), data.password);
+  userEvent.click(screen.getByRole('button', { name: 'Login' }));
+
+  await waitFor(() =>
+    expect(screen.getByRole('button', { name: 'Login' })).not.toBeDisabled(),
+  );
+
+  expect(handleSubmitSpy).toHaveBeenCalledWith(data);
+});
+
+test('validate login form', async () => {
+  const handleSubmitSpy = jest
+    .fn()
+    .mockImplementation(
+      () =>
+        new Promise((resolve) =>
+          setTimeout(resolve, Math.round(Math.random() * 1e3)),
+        ),
+    );
+  const data = loginBuild({ traits: 'invalid' });
+
+  render(<LoginForm onSubmit={handleSubmitSpy} />);
+
+  userEvent.click(screen.getByRole('button', { name: 'Login' }));
+
+  await expect(
+    screen.findByText(validations.email.required),
+  ).resolves.toBeInTheDocument();
+  await expect(
+    screen.findByText(validations.password.required),
+  ).resolves.toBeInTheDocument();
+
+  userEvent.type(screen.getByLabelText(/Email/i), data.email);
+  userEvent.type(screen.getByLabelText(/Password/i), data.password);
+  userEvent.click(screen.getByRole('button', { name: 'Login' }));
+
+  await expect(
+    screen.findByText(validations.email.pattern.message),
+  ).resolves.toBeInTheDocument();
+  await expect(
+    screen.findByText(validations.password.minLength.message),
+  ).resolves.toBeInTheDocument();
+
+  expect(handleSubmitSpy).not.toHaveBeenCalled();
+});
--- src/testUtils.tsx
+++ src/testUtils.tsx
@@ -1,8 +1,10 @@
 import { ChakraProvider, theme } from '@chakra-ui/react';
-import { render, screen } from '@testing-library/react';
+import { build, fake } from '@jackfranklin/test-data-bot';
+import { render, screen, waitFor } from '@testing-library/react';
 
 import { AuthProvider } from './hooks/useAuth';
 import type { AuthContext } from './types/AuthContext';
+import type { Login } from './types/Login';
 
 function wrapWithProviders(context: AuthContext = {} as AuthContext) {
   return ({ children }: React.PropsWithChildren<{}>) => (
@@ -15,4 +17,20 @@ function wrapWithProviders(context: AuthContext = {} as AuthContext) {
 function customRender(ui: Parameters<typeof render>[0], context?: AuthContext) {
   return render(ui, { wrapper: wrapWithProviders(context) });
 }
-export { customRender as render, screen };
+
+const loginBuild = build<Login>({
+  fields: {
+    email: fake((f) => f.internet.email()),
+    password: fake((f) => f.internet.password(12)),
+  },
+  traits: {
+    invalid: {
+      overrides: {
+        email: fake((f) => f.internet.userName()),
+        password: fake((f) => f.internet.password(7)),
+      },
+    },
+  },
+});
+
+export { customRender as render, screen, waitFor, loginBuild };

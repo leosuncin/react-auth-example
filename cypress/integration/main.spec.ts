54cf730c70dbed74e016b03d0e7632fe79afc381
Jaime Leonardo Suncin Cruz <leosuncin@gmail.com>
2021-04-03 00:33:57 -0600

    test(e2e): Unauthenticated and authenticated mode
    Restore state from local storage.
    Log out.

--- cypress/integration/main.spec.ts
+++ cypress/integration/main.spec.ts
@@ -1,3 +1,69 @@
-it('opens the app', () => {
-  cy.visit('/').get('nav h1').should('be.visible');
+import { userBuild } from '../../src/testUtils';
+import { AuthStorageEnum } from '../../src/types/AuthStorageEnum';
+
+context('Unauthenticated', () => {
+  beforeEach(() => {
+    cy.visit('/');
+    cy.clearLocalStorage();
+  });
+
+  it('shows app title', () => {
+    cy.findByRole('heading', { name: 'React App' }).should('be.visible');
+    cy.findByText('Log out').should('not.exist');
+  });
+
+  it('shows tabs with form', () => {
+    cy.findByRole('tab', { name: 'Register' }).should(
+      'have.attr',
+      'aria-selected',
+      'true',
+    );
+    cy.findByRole('tab', { name: 'Login' }).should(
+      'have.attr',
+      'aria-selected',
+      'false',
+    );
+    cy.findByTestId('register').should('be.visible');
+    cy.findByTestId('login').should('not.be.visible');
+
+    cy.findByRole('tab', { name: 'Login' }).click();
+
+    cy.findByRole('tab', { name: 'Register' }).should(
+      'have.attr',
+      'aria-selected',
+      'false',
+    );
+    cy.findByRole('tab', { name: 'Login' }).should(
+      'have.attr',
+      'aria-selected',
+      'true',
+    );
+    cy.findByTestId('register').should('not.be.visible');
+    cy.findByTestId('login').should('be.visible');
+  });
+});
+
+context('Authenticated', () => {
+  const user = userBuild();
+
+  beforeEach(() => {
+    cy.visit('/');
+    const token = btoa(JSON.stringify(user));
+    localStorage.setItem(AuthStorageEnum.token, token);
+    localStorage.setItem(AuthStorageEnum.user, JSON.stringify(user));
+  });
+
+  it('show user details', () => {
+    cy.findByRole('img', { name: user.name });
+    cy.findByText(user.name);
+  });
+
+  it('log out', () => {
+    cy.findByRole('button', { name: 'Log out' })
+      .click()
+      .should(() => {
+        assert.isNull(localStorage.getItem(AuthStorageEnum.token));
+        assert.isNull(localStorage.getItem(AuthStorageEnum.user));
+      });
+  });
 });

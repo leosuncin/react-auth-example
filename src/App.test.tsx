85d4ccc9e289238cfcf17757fde313b20f4fb12d
Jaime Leonardo Suncin Cruz <leosuncin@gmail.com>
2021-04-04 03:32:58 -0600

    test: Mock network request with MSW

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,10 +1,152 @@
-import { render, screen } from '@testing-library/react';
+import { queries, render, screen, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { setupServer } from 'msw/node';
 
 import App from './App';
+import {
+  loginBuild,
+  loginHandler,
+  registerBuild,
+  registerHandler,
+  userBuild,
+} from './testUtils';
+import { AuthStorageEnum } from './types/AuthStorageEnum';
+
+const server = setupServer(registerHandler, loginHandler);
+
+beforeAll(() => {
+  server.listen();
+});
+
+beforeEach(() => {
+  localStorage.clear();
+});
+
+afterAll(() => {
+  server.close();
+});
 
 test('renders "React App" in App', () => {
   render(<App />);
 
   const element = screen.getByText(/React App/i);
   expect(element).toBeInTheDocument();
+});
+
+test('register a new user', async () => {
+  const data = registerBuild();
+
+  render(<App />);
+
+  userEvent.type(
+    queries.getByLabelText(screen.getByTestId('register'), /Name/i),
+    data.name,
+  );
+  userEvent.type(
+    queries.getByLabelText(screen.getByTestId('register'), /Email/i),
+    data.email,
+  );
+  userEvent.type(
+    queries.getByLabelText(screen.getByTestId('register'), /Password/i),
+    data.password,
+  );
+  userEvent.click(queries.getByRole(screen.getByTestId('register'), 'button'));
+
+  await waitFor(() =>
+    expect(screen.queryByTestId('register')).not.toBeInTheDocument(),
+  );
+
+  expect(screen.getByRole('img', { name: data.name })).toBeInTheDocument();
+  expect(screen.getByText(data.name)).toBeInTheDocument();
+});
+
+test('show register error message', async () => {
+  const data = registerBuild({ overrides: { email: 'john@doe.me' } });
+
+  render(<App />);
+
+  userEvent.type(
+    queries.getByLabelText(screen.getByTestId('register'), /Name/i),
+    data.name,
+  );
+  userEvent.type(
+    queries.getByLabelText(screen.getByTestId('register'), /Email/i),
+    data.email,
+  );
+  userEvent.type(
+    queries.getByLabelText(screen.getByTestId('register'), /Password/i),
+    data.password,
+  );
+  userEvent.click(queries.getByRole(screen.getByTestId('register'), 'button'));
+
+  await expect(
+    screen.findByText('The email «john@doe.me» is already register.'),
+  ).resolves.toBeInTheDocument();
+});
+
+test('login with a user', async () => {
+  const data = loginBuild({ overrides: { email: 'john@doe.me' } });
+
+  render(<App />);
+
+  userEvent.click(screen.getByRole('tab', { name: 'Login' }));
+
+  userEvent.type(
+    queries.getByLabelText(screen.getByTestId('login'), /Email/i),
+    data.email,
+  );
+  userEvent.type(
+    queries.getByLabelText(screen.getByTestId('login'), /Password/i),
+    data.password,
+  );
+  userEvent.click(queries.getByRole(screen.getByTestId('login'), 'button'));
+
+  await waitFor(() =>
+    expect(screen.queryByTestId('login')).not.toBeInTheDocument(),
+  );
+
+  expect(screen.getByRole('img')).toBeInTheDocument();
+});
+
+test('show login error message', async () => {
+  const data = loginBuild();
+
+  render(<App />);
+
+  userEvent.click(screen.getByRole('tab', { name: 'Login' }));
+
+  userEvent.type(
+    queries.getByLabelText(screen.getByTestId('login'), /Email/i),
+    data.email,
+  );
+  userEvent.type(
+    queries.getByLabelText(screen.getByTestId('login'), /Password/i),
+    data.password,
+  );
+  userEvent.click(queries.getByRole(screen.getByTestId('login'), 'button'));
+
+  await expect(
+    screen.findByText(`There isn't any user with email: ${data.email}`),
+  ).resolves.toBeInTheDocument();
+});
+
+test('log out', async () => {
+  const user = userBuild();
+  const token = btoa(JSON.stringify(user));
+
+  localStorage.setItem(AuthStorageEnum.user, JSON.stringify(user));
+  localStorage.setItem(AuthStorageEnum.token, token);
+  render(<App />);
+
+  expect(screen.getByRole('img', { name: user.name })).toBeInTheDocument();
+  expect(screen.getByText(user.name)).toBeInTheDocument();
+
+  userEvent.click(screen.getByText('Log out'));
+
+  await waitFor(() =>
+    expect(screen.queryByText('Log out')).not.toBeInTheDocument(),
+  );
+
+  expect(localStorage.getItem(AuthStorageEnum.user)).toBeNull();
+  expect(localStorage.getItem(AuthStorageEnum.token)).toBeNull();
 });
--- src/testUtils.tsx
+++ src/testUtils.tsx
@@ -1,6 +1,7 @@
 import { ChakraProvider, theme } from '@chakra-ui/react';
 import { build, fake, perBuild, sequence } from '@jackfranklin/test-data-bot';
 import { render } from '@testing-library/react';
+import { rest } from 'msw';
 
 import { AuthProvider } from './hooks/useAuth';
 import type { AuthContext } from './types/AuthContext';
@@ -21,6 +22,7 @@ function customRender(ui: Parameters<typeof render>[0], context?: AuthContext) {
 }
 
 export { screen, waitFor } from '@testing-library/react';
+export { customRender as render };
 
 export const loginBuild = build<Login>({
   fields: {
@@ -64,4 +66,58 @@ export const userBuild = build<User>({
   },
 });
 
-export { customRender as render };
+export const registerHandler = rest.post<Register, User>(
+  `${process.env.REACT_APP_API_URL}/auth/register`,
+  (request, response, context) => {
+    const { email, name } = request.body;
+    const user = userBuild({
+      overrides: { name, email },
+    });
+    const token = Buffer.from(JSON.stringify(user)).toString('base64');
+
+    if (email === 'john@doe.me')
+      return response(
+        context.status(422),
+        context.json({
+          error: 'Unprocessable Entity',
+          message: ['The email «john@doe.me» is already register.'],
+          statusCode: 422,
+        } as any),
+      );
+
+    return response(
+      context.delay(Math.round(Math.random() * 1e3)),
+      context.status(201),
+      context.set('Authorization', `Bearer ${token}`),
+      context.cookie('token', token, { httpOnly: true, sameSite: 'strict' }),
+      context.json(user),
+    );
+  },
+);
+
+export const loginHandler = rest.post<Login, User>(
+  `${process.env.REACT_APP_API_URL}/auth/login`,
+  (request, response, context) => {
+    const { email } = request.body;
+    const user = userBuild({ overrides: { email } });
+    const token = Buffer.from(JSON.stringify(user)).toString('base64');
+
+    if (email !== 'john@doe.me')
+      return response(
+        context.status(401),
+        context.json({
+          error: 'Unauthorized',
+          message: `There isn't any user with email: ${email}`,
+          statusCode: 401,
+        } as any),
+      );
+
+    return response(
+      context.delay(Math.round(Math.random() * 1e3)),
+      context.status(200),
+      context.set('Authorization', `Bearer ${token}`),
+      context.cookie('token', token, { httpOnly: true, sameSite: 'strict' }),
+      context.json(user),
+    );
+  },
+);
